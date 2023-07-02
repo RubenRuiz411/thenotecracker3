df47ac018f9b38a3969223c45e0698270bf85bd4
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-07-02 11:15:46 -0700

    added adjustment to PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const path = require('path');
 const app = express();
 const uniqid = require('uniqid');
